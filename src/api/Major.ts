323679acc37271629058380b75e522cfc37e50a5
Baris <2501920571@qq.com>
2025-06-20 14:33:35 +0800

    分段模块完成

--- src/api/Major.ts
+++ src/api/Major.ts
@@ -3,13 +3,14 @@ import Serveraxios from '@/api/instance/index'
 export const GetSchool = (year: string , subject: string,is985:string,pici :string,province:string,prefecture:string,county:string) => {
     return Serveraxios.get(`Major/School?year=${year}&subject=${subject}&is985=${is985}&pici=${pici}&province=${province}&prefecture=${prefecture}&county=${county}`)
 }
-
 export const GetPlan = (year: string , subject: string) => {
     return Serveraxios.get(`Major/GetPlan?year=${year}&subject=${subject}`)
 }
-
 export const GetSubject = (year: string , subject: string) => {
     return Serveraxios.get(`Major/GetSubject?year=${year}&subject=${subject}`)
+}
+export const GetRank = (year: string , subject: string) => {
+    return Serveraxios.get(`Major/GetRank?year=${year}&subject=${subject}`)
 }
 
 export const GetLogoUrl = (name: string) => {
